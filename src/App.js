0512ba28346c71ab75036a2dba8efa45aa9ef01a
Nick <nickdekok.nl@gmail.com>
2023-06-02 14:42:24 +0200

    add routes to all pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Navigate, Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes, useParams} from "react-router-dom";
 import './App.css';
 
 //pages
@@ -19,15 +19,37 @@ import Volunteers from "./pages/Volunteers/Volunteers";
 function App() {
     const isAuth = true;
 
+    function eventPage() {
+        const { event_id } = useParams
+    }
+
+    function taskPage() {
+        const { task_id } = useParams
+    }
+
+    function volunteerPage() {
+        const { volunteer_id } = useParams
+    }
+
   return (
     <>
         <main>
             <Routes>
                 <Route path="/login" element={<Login/>}/>
-                <Route path="/signup" element/>
-                <Route path="/forgotpassword"/>
+                <Route path="/signup" element={<SignUp/>}/>
+                <Route path="/forgot_password" element={<ForgotPassword/>}/>
+
                 {/* Authorized pages */}
-                <Route path="/login"/>
+                <Route path="/profile" element={isAuth ? <Profile/> : <Navigate to="login"/>}/>
+                <Route path="/database" element={isAuth ? <Database/> : <Navigate to="login"/>}/>
+                <Route path="/calendar" element={isAuth ? <Calendar/> : <Navigate to="login"/>}/>
+                <Route path="/new_event" element={isAuth ? <NewEvent/> : <Navigate to="login"/>}/>
+                <Route path="/event/:event_id" element={isAuth ? <Event/> : <Navigate to="login"/>}/>
+                <Route path="/tasks" element={isAuth ? <Tasks/> : <Navigate to="login"/>}/>
+                <Route path="/new_task" element={isAuth ? <NewTask/> : <Navigate to="login"/>}/>
+                <Route path="/task/:task_id" element={isAuth ? <Task/> : <Navigate to="login"/>}/>
+                <Route path="/volunteers" element={isAuth ? <Volunteers/> : <Navigate to="login"/>}/>
+                <Route path="/volunteer/:user_id" element={isAuth ? <Volunteer/> : <Navigate to="login"/>}/>
             </Routes>
         </main>
     </>
