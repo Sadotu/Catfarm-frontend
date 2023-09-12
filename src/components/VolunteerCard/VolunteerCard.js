ed5f77f66cf510bcfc7671ee0624d4750b83532e
Nick <nickdekok.nl@gmail.com>
2023-09-12 07:25:16 +0200

    add styling to volunteers page

--- src/components/VolunteerCard/VolunteerCard.js
+++ src/components/VolunteerCard/VolunteerCard.js
@@ -5,18 +5,20 @@ import User from "../../assets/icons/user.svg";
 function VolunteerCard( {volunteer} ) {
     return (
         <div className="user-card-inner-container">
-            {volunteer.profilePicture && volunteer.profilePicture.docFile ? (
-                <img className="volunteer-picture" src={`data:image/jpeg;base64,${volunteer.profilePicture.docFile}`} alt={volunteer.name} />
-            ) : (
-                <img src={User}></img>
-            )}
-            <div className="volunteer-name">
-                <h4>{volunteer.fullName}</h4>
-                <h4>{volunteer.pronouns}</h4>
-            </div>
-            <div className="volunteer-info">
-                <h5>{volunteer.email}</h5>
-                <h5>{volunteer.phoneNumber}</h5>
+            <div className="volunteer-card">
+                {volunteer.profilePicture && volunteer.profilePicture.docFile ? (
+                    <img className="volunteer-picture" src={`data:image/jpeg;base64,${volunteer.profilePicture.docFile}`} alt={volunteer.name} />
+                ) : (
+                    <img className="volunteer-icon" src={User}></img>
+                )}
+                <div className="volunteer-name">
+                    <h4>{volunteer.fullName}</h4>
+                    <h4>{volunteer.pronouns}</h4>
+                </div>
+                <div className="volunteer-info">
+                    <h5>{volunteer.email}</h5>
+                    <h5>{volunteer.phoneNumber}</h5>
+                </div>
             </div>
         </div>
     );
